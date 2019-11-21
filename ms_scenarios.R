69513b41ba746839315db265a41a0505fca2c07e
bsmity13 <bsmity13@gmail.com>
2019-11-21 15:08:56 -0700

    Updated scenarios and created figures for scenarios 1 -- 8.

--- ms_scenarios.R
+++ ms_scenarios.R
@@ -41,11 +41,17 @@ p1 <- plot_sim(sc1) +
   geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
   annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250)) +
   geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500))
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500))+
+  annotate(geom = "label", x = 165, y=450, 
+           label = "Base Scenario", 
+           fontface = "italic", size = 3)
 
 #Zoom
 p1z <- plot_sim(sc1) +
-  coord_cartesian(xlim = c(0, 200), ylim=c(0, 55))
+  coord_cartesian(xlim = c(0, 200), ylim=c(0, 55))+
+  annotate(geom = "label", x = 165, y=50, 
+           label = "Base Scenario\n(zoomed)", 
+           fontface = "italic", size = 3)
 
 #Legend
 leg <- get_legend(
@@ -80,13 +86,16 @@ p2 <- plot_sim(sc2) +
   geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
   annotate(geom = "text", x = 100, y = 220, label = expression(K[A] * "=" * 250)) +
   geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 100, y = 470, label = expression(K[B] * "=" * 500))
+  annotate(geom = "text", x = 100, y = 470, label = expression(K[B] * "=" * 500)) +
+  annotate(geom = "label", x = 165, y=475, 
+           label = "No Habitat\nPreference", 
+           fontface = "italic", size = 3)
 
 #Zoom
 p2z <- plot_sim(sc2) +
   coord_cartesian(ylim=c(0, 55))
 
-#Low preference
+#Weak preference
 sc3 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
                   r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
                   T_HH = c(0.6, 0.4), alpha_H = base_alpha_H, 
@@ -95,9 +104,12 @@ p3 <- plot_sim(sc3) +
   geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
   annotate(geom = "text", x = 100, y = 470, label = expression(K[B] * "=" * 500)) +
   geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 170, y = 220, label = expression(K[A] * "=" * 250))
+  annotate(geom = "text", x = 170, y = 220, label = expression(K[A] * "=" * 250)) +
+  annotate(geom = "label", x = 165, y=380, 
+           label = "Weak Habitat\nPreference", 
+           fontface = "italic", size = 3)
 
-#High preference for B
+#Strong prop preference for B
 sc4 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
                   r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
                   T_HH = c(0.1, 0.9), alpha_H = base_alpha_H, 
@@ -109,7 +121,10 @@ p4 <- plot_sim(sc4) +
   annotate(geom = "text", x = 170, y = 230, label = expression(K[A] * "=" * 250))
 
 p4z <- plot_sim(sc4) +
-  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50))
+  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50)) +
+  annotate(geom = "label", x = 155, y=47, 
+           label = "Strong Proportional\nHabitat Preference", 
+           fontface = "italic", size = 3)
 
 #High preference for A
 sc5 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
@@ -124,7 +139,10 @@ p5 <- plot_sim(sc5) +
   annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250))
 
 p5z <- plot_sim(sc5) +
-  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50))
+  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50))+
+  annotate(geom = "label", x = 165, y=45, 
+           label = "Strong Inverse\nHabitat Preference", 
+           fontface = "italic", size = 3)
 
 #Combine
 row23 <- plot_grid(
@@ -132,17 +150,15 @@ row23 <- plot_grid(
   p3 + 
     theme(legend.position = "none",
           axis.text.y = element_blank()) +
-    ylab(NULL),
-  labels = c("a", "b"),
-  label_x = c(0, -0.04))
+    ylab(" "),
+  labels = c("a", "b"))
 row45 <- plot_grid(
   p4z + theme(legend.position = "none"), 
   p5z + 
     theme(legend.position = "none",
           axis.text.y = element_blank()) +
-    ylab(NULL),
-  labels = c("c", "d"),
-  label_x = c(0, -0.04))
+    ylab(" "),
+  labels = c("c", "d"))
 
 final2345 <- plot_grid(
   row23, row45, leg,
@@ -152,74 +168,53 @@ final2345 <- plot_grid(
 ggsave(plot = final2345, filename = "fig/sc2-5.tiff", width = 6.5, height = 5.5, units = "in",
        dpi = 200, compression = "lzw")
 
-#Carrying Capacity Scenarios----
-#Proportional K
-sc4 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.03), 
-                  K_H = c(50, 200), kappa_H = c(25, 100))
-#Zoom
-p4z <- plot_sim(sc4) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 70))
-
-#Inverse K
-sc5 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.03), 
-                  K_H = c(50, 200), kappa_H = c(100, 25))
-#Zoom
-p5z <- plot_sim(sc5) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 70))
-
-#Combine
-row45 <- plot_grid(
-  p4z + theme(legend.position = "none"), 
-  p5z + 
-    theme(legend.position = "none") +
-    ylab(NULL),
-  labels = "auto")
-
-final45 <- plot_grid(
-  row45, leg,
-  ncol = 1, rel_heights = c(1, .1)
-)
-
 #Attack Rate Scenarios----
-#Low Attack Difference
-sc6 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.25, 0.25), alpha_H = c(0.025, 0.035), 
-                  K_H = c(50, 200), kappa_H = c(25, 25))
+#Low Inverse Attack Difference
+sc6 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = base_T_HH, alpha_H = c(0.02, 0.01), 
+                  K_H = base_K_H, base_kappa_H)
 #Zoom
-p6z <- plot_sim(sc6) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 70))
+p6 <- plot_sim(sc6) +
+  annotate(geom = "label", x = 165, y=225, 
+           label = "Low Inverse\nAttack Difference", 
+           fontface = "italic", size = 3)
 
-#High Attack Difference
-sc7 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.25, 0.25), alpha_H = c(0.01, 0.05), 
-                  K_H = c(50, 200), kappa_H = c(25, 25))
+#High Proportional Attack Difference
+sc7 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = base_T_HH, alpha_H = c(0.005, 0.025), 
+                  K_H = base_K_H, kappa_H = base_kappa_H)
 #Zoom
-p7z <- plot_sim(sc7) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 70))
+p7 <- plot_sim(sc7) +
+  annotate(geom = "label", x = 165, y=225, 
+           label = "High Proportional\nAttack Difference", 
+           fontface = "italic", size = 3)
+
+#High Inverse Attack Difference
+sc8 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = base_T_HH, alpha_H = c(0.025, 0.005), 
+                  K_H = base_K_H, base_kappa_H)
+
+#Zoom
+p8 <- plot_sim(sc8) +
+  annotate(geom = "label", x = 165, y=200, 
+           label = "High Inverse\nAttack Difference", 
+           fontface = "italic", size = 3)
 
 #Combine
-row67 <- plot_grid(
-  p6z + theme(legend.position = "none"), 
-  p7z + 
-    theme(legend.position = "none") +
-    ylab(NULL),
-  labels = "auto")
+final678 <- plot_grid(
+  p6 + 
+    theme(legend.position = "none"), 
+  p7 + 
+    theme(legend.position = "none",
+          axis.text.y = element_blank()) +
+    ylab(" "),
+  p8 + 
+    theme(legend.position = "none"),
+  leg,
+  labels = c("a", "b", "c", NA))
 
-final67 <- plot_grid(
-  row67, leg,
-  ncol = 1, rel_heights = c(1, .1)
-)
+ggsave(plot = final678, filename = "fig/sc6-8.tiff", width = 6.5, height = 5.5, units = "in",
+       dpi = 200, compression = "lzw")
