30e2792891a346643e19719d903d4b75f37600e3
bsmity13 <34109166+bsmity13@users.noreply.github.com>
2019-11-20 23:23:03 -0700

    Updated manuscript scenarios 1-5

--- ms_scenarios.R
+++ ms_scenarios.R
@@ -20,23 +20,32 @@ library(tidyr)
 library(ggplot2)
 library(cowplot)
 
+#Base Parameters----
+base_N_Ht = c(250, 250)
+base_rho_Ht = c(25, 25)
+base_r = 0.1
+base_epsilon = c(0.4, 0.4)
+base_phi_H = c(0.9, 0.9)
+base_T_HH = c(0, 0)
+base_alpha_H = c(0.01, 0.02)
+base_K_H = c(250, 500)
+base_kappa_H = c(25, 50)
+
 #Base Scenario----
-sc1 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-           r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-           T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.03), 
-           K_H = c(50, 200), kappa_H = c(25, 25))
+sc1 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = base_T_HH, alpha_H = base_alpha_H, 
+                  K_H = base_K_H, base_kappa_H)
 
 p1 <- plot_sim(sc1) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[B] * "=" * 200)) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 40, label = expression(K[A] * "=" * 50))
-  
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250)) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500))
+
 #Zoom
 p1z <- plot_sim(sc1) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 55))
+  coord_cartesian(xlim = c(0, 200), ylim=c(0, 55))
 
 #Legend
 leg <- get_legend(
@@ -58,53 +67,90 @@ final1 <- plot_grid(
   ncol = 1, rel_heights = c(1, .1)
 )
 
+ggsave(plot = final1, filename = "fig/sc1.tiff", width = 6.5, height = 3.5, units = "in",
+       dpi = 200, compression = "lzw")
+
 #Habitat Selection Scenarios----
-#Low preference
-sc2 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.2, 0.3), alpha_H = c(0.03, 0.03), 
-                  K_H = c(50, 200), kappa_H = c(25, 25))
+#No preference
+sc2 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = c(0.5, 0.5), alpha_H = base_alpha_H, 
+                  K_H = base_K_H, base_kappa_H)
 p2 <- plot_sim(sc2) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[B] * "=" * 200)) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 40, label = expression(K[A] * "=" * 50))
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 220, label = expression(K[A] * "=" * 250)) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 470, label = expression(K[B] * "=" * 500))
 
 #Zoom
 p2z <- plot_sim(sc2) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
   coord_cartesian(ylim=c(0, 55))
 
-#High preference
-sc3 <- sim_system(T = 600, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                  r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                  T_HH = c(0.1, 0.5), alpha_H = c(0.03, 0.03), 
-                  K_H = c(50, 200), kappa_H = c(25, 25))
+#Low preference
+sc3 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = c(0.6, 0.4), alpha_H = base_alpha_H, 
+                  K_H = base_K_H, base_kappa_H)
 p3 <- plot_sim(sc3) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[B] * "=" * 200)) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 40, label = expression(K[A] * "=" * 50))
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 470, label = expression(K[B] * "=" * 500)) +
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 170, y = 220, label = expression(K[A] * "=" * 250))
 
-#Zoom
-p3z <- plot_sim(sc3) +
-  geom_hline(yintercept = 50, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 48, label = expression(K[A] * "=" * 50)) +
-  coord_cartesian(ylim=c(0, 55))
+#High preference for B
+sc4 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = c(0.1, 0.9), alpha_H = base_alpha_H, 
+                  K_H = base_K_H, base_kappa_H)
+p4 <- plot_sim(sc4) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500)) +
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 170, y = 230, label = expression(K[A] * "=" * 250))
+
+p4z <- plot_sim(sc4) +
+  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50))
+
+#High preference for A
+sc5 <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                  r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                  T_HH = c(0.9, 0.1), alpha_H = base_alpha_H, 
+                  K_H = base_K_H, base_kappa_H)
+
+p5 <- plot_sim(sc5) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500)) +
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250))
+
+p5z <- plot_sim(sc5) +
+  coord_cartesian(xlim = c(0, 200), ylim = c(0, 50))
 
 #Combine
 row23 <- plot_grid(
-  p2z + theme(legend.position = "none"), 
-  p3z + 
-    theme(legend.position = "none") +
+  p2 + theme(legend.position = "none"), 
+  p3 + 
+    theme(legend.position = "none",
+          axis.text.y = element_blank()) +
     ylab(NULL),
-  labels = "auto")
+  labels = c("a", "b"),
+  label_x = c(0, -0.04))
+row45 <- plot_grid(
+  p4z + theme(legend.position = "none"), 
+  p5z + 
+    theme(legend.position = "none",
+          axis.text.y = element_blank()) +
+    ylab(NULL),
+  labels = c("c", "d"),
+  label_x = c(0, -0.04))
 
-final23 <- plot_grid(
-  row23, leg,
-  ncol = 1, rel_heights = c(1, .1)
+final2345 <- plot_grid(
+  row23, row45, leg,
+  ncol = 1, rel_heights = c(1, 1, .1)
 )
+
+ggsave(plot = final2345, filename = "fig/sc2-5.tiff", width = 6.5, height = 5.5, units = "in",
+       dpi = 200, compression = "lzw")
 
 #Carrying Capacity Scenarios----
 #Proportional K
