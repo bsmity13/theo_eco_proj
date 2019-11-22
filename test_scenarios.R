bf168d8f568f43d2f98711dfc4253a15ca2f3407
bsmity13 <34109166+bsmity13@users.noreply.github.com>
2019-11-21 21:01:15 -0700

    Updated test_scenarios to just those tests that would appear in the supplement of the first draft ms.

--- test_scenarios.R
+++ test_scenarios.R
@@ -3,9 +3,9 @@
 #----------------Brian J. Smith----------------X
 #------------------Fall 2019-------------------X
 #==============================================X
-#------------------Scenarios-------------------X
+#----------------Test Scenarios----------------X
 #==============================================X
-#------------Last update 2019-11-13------------X
+#------------Last update 2019-11-21------------X
 ###############################################X
 
 #Set options----
@@ -18,173 +18,109 @@ source("proj_fun.R")
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(cowplot)
 
-#Base scenario, no predators----
-basic_nopred <- sim_system(T = 200, N_Ht = c(100, 100), rho_Ht = c(0, 0), 
-                      r = 0.1, epsilon = c(0, 0), phi_H = c(0, 0), 
-                      T_HH = c(0.25, 0.25), alpha_H = c(0, 0), 
-                      K_H = c(200, 200), kappa_H = c(20, 20))
-plot_sim(basic_nopred) +
-  annotate(geom = "text", x = 100, y = 191, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  ggtitle("Base Scenario, No Predators")
+#Base Parameters----
+base_N_Ht = c(250, 250)
+base_rho_Ht = c(25, 25)
+base_r = 0.1
+base_epsilon = c(0.4, 0.4)
+base_phi_H = c(0.9, 0.9)
+base_T_HH = c(0, 0)
+base_alpha_H = c(0.01, 0.02)
+base_K_H = c(250, 500)
+base_kappa_H = c(25, 50)
 
-#Base Scenario, with predators----
-basic_pred <- sim_system(T = 750, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-           r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-           T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.03), 
-           K_H = c(200, 200), kappa_H = c(25, 25))
-
-plot_sim(basic_pred) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  ggtitle("Base Scenario, with Predators")
-  
-#Zoom
-plot_sim(basic_pred) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  coord_cartesian(ylim=c(0, 80)) +
-  ggtitle("Base Scenario, with Predators (zoomed)")
-
-#Different K, no predators----
-preyK_nopred <- sim_system(T = 250, N_Ht = c(100, 100), rho_Ht = c(0, 0), 
-                      r = 0.1, epsilon = c(0, 0), phi_H = c(0, 0), 
-                      T_HH = c(0.25, 0.25), alpha_H = c(0, 0), 
-                      K_H = c(50, 200), kappa_H = c(20, 20))
-
-plot_sim(preyK_nopred) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 250/2, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 250/2, y = 190, label = expression(K[B] * " = " * 200)) +
-  ggtitle("Movement between Habitats")
-
-#Different K, no predators, no movement between habitats----
-preyK_nopred_nohab <- sim_system(T = 200, N_Ht = c(100, 100), rho_Ht = c(0, 0), 
-                                 r = 0.1, epsilon = c(0, 0), phi_H = c(0, 0), 
-                                 T_HH = c(0, 0), alpha_H = c(0, 0), 
-                                 K_H = c(50, 200), kappa_H = c(20, 20))
-
-plot_sim(preyK_nopred_nohab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 100, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 100, y = 190, label = expression(K[B] * " = " * 200)) +
-  ggtitle("No Movement Between Habitats")
-
-#Different K, no predators, habitat selection prop to K----
-#This seems like the ideal free distribution
-preyK_nopred_prophab <- sim_system(T = 200, N_Ht = c(100, 100), rho_Ht = c(0, 0), 
-                                 r = 0.1, epsilon = c(0, 0), phi_H = c(0, 0), 
-                                 T_HH = c(0.4, 0.1), alpha_H = c(0, 0), 
-                                 K_H = c(50, 200), kappa_H = c(20, 20))
-
-plot_sim(preyK_nopred_prophab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 100, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 100, y = 190, label = expression(K[B] * " = " * 200)) +
-  ggtitle("Habitat Selection Proportional to K")
+#No predator test----
+#Base scenario, no predators, no movement
+basic_nopred <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = c(0, 0), 
+                           r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                           T_HH = base_T_HH, alpha_H = base_alpha_H, 
+                           K_H = base_K_H, base_kappa_H)
+supp1a <- plot_sim(basic_nopred) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500)) +
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250)) +
+  coord_cartesian(ylim = c(0, 500)) +
+  annotate(geom = "label", x = 100, y=100, 
+           label = "No Predators\nNo Movement", 
+           fontface = "italic", size = 3)
 
 
-#Different K, with predators----
-preyK_pred <- sim_system(T = 400, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                           r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                           T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.03), 
-                           K_H = c(50, 200), kappa_H = c(20, 20))
-plot_sim(preyK_pred) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 200, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 200, y = 190, label = expression(K[B] * " = " * 200))
-#Zoom
-plot_sim(preyK_pred) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 200, y = 48, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 200, y = 190, label = expression(K[B] * " = " * 200)) +
-  coord_cartesian(ylim=c(0, 55)) +
-  ggtitle("Equal Movement between Habitats")
 
-#Different K, with predators, no movement between habitats----
-preyK_pred_nohab <- sim_system(T = 500, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                         r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                         T_HH = c(0, 0), alpha_H = c(0.03, 0.03), 
-                         K_H = c(50, 200), kappa_H = c(20, 20))
-plot_sim(preyK_pred_nohab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 200, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 200, y = 190, label = expression(K[B] * " = " * 200))
-#Zoom
-plot_sim(preyK_pred_nohab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 200, y = 48, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 200, y = 190, label = expression(K[B] * " = " * 200)) +
-  coord_cartesian(ylim=c(0, 70)) +
-  ggtitle("No Movement Between Habitats")
+#Base scenario, no predators, movement
+basic_nopred_move <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = c(0, 0), 
+                           r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                           T_HH = c(0.25, 0.25), alpha_H = base_alpha_H, 
+                           K_H = base_K_H, base_kappa_H)
 
-#Different K, with predators, proportional habitat selection----
-preyK_pred_prophab <- sim_system(T = 500, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                               r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                               T_HH = c(0.4, 0.1), alpha_H = c(0.02, 0.02), 
-                               K_H = c(50, 200), kappa_H = c(20, 20))
-plot_sim(preyK_pred_prophab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 250, y = 40, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 250, y = 190, label = expression(K[B] * " = " * 200))
-#Zoom
-plot_sim(preyK_pred_prophab) +
-  geom_hline(yintercept = c(50, 200), linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 250, y = 48, label = expression(K[A] * " = " * 50)) +
-  annotate(geom = "text", x = 250, y = 190, label = expression(K[B] * " = " * 200)) +
-  coord_cartesian(ylim=c(0, 60)) +
-  ggtitle("Habitat Selection Proportional to K")
+supp1b <- plot_sim(basic_nopred_move) +
+  geom_hline(yintercept = 500, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 480, label = expression(K[B] * "=" * 500)) +
+  geom_hline(yintercept = 250, linetype = "dashed", color = "gray40", size = 1) +
+  annotate(geom = "text", x = 100, y = 230, label = expression(K[A] * "=" * 250)) +
+  coord_cartesian(ylim = c(0, 500)) +
+  annotate(geom = "label", x = 100, y=100, 
+           label = "No Predators\nEqual Movement", 
+           fontface = "italic", size = 3)
 
-#Different attack rates, high movement----
-attack_himove <- sim_system(T = 750, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                          r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                          T_HH = c(0.25, 0.25), alpha_H = c(0.03, 0.06), 
-                          K_H = c(200, 200), kappa_H = c(25, 25))
+#Legend
+leg <- get_legend(
+  supp1a + 
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
 
-plot_sim(attack_himove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  ggtitle("Different Attack Rates, with High Movement")
+#Combine
+row1 <- plot_grid(
+  supp1a + theme(legend.position = "none"), 
+  supp1b + 
+    theme(legend.position = "none") +
+    ylab(" "),
+  labels = "auto")
 
-#Zoom
-plot_sim(attack_himove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  coord_cartesian(ylim=c(0, 80)) +
-  ggtitle("Different Attack Rates, with High Movement (zoomed)")
+supp1 <- plot_grid(
+  row1, leg,
+  ncol = 1, rel_heights = c(1, .1)
+)
 
-#Different attack rates, low movement----
-attack_lomove <- sim_system(T = 750, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                          r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                          T_HH = c(0.05, 0.05), alpha_H = c(0.03, 0.06), 
-                          K_H = c(200, 200), kappa_H = c(25, 25))
+ggsave(plot = supp1, filename = "fig/supp1.tiff", width = 6.5, height = 3.5, units = "in",
+       dpi = 200, compression = "lzw")
 
-plot_sim(attack_lomove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  ggtitle("Different Attack Rates, with Low Movement")
+#Stable predator attack rate test----
+attack_nomove <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                            r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                            T_HH = base_T_HH, alpha_H = c(0.01, 0.01), 
+                            K_H = base_K_H, base_kappa_H)
+supp2a <- plot_sim(attack_nomove) +
+  coord_cartesian(ylim=c(0, 100)) +
+  annotate(geom = "label", x = 150, y=90, 
+           label = "Equal Stable Attack Rate\nNo Movement", 
+           fontface = "italic", size = 3)
 
-#Zoom
-plot_sim(attack_lomove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  coord_cartesian(ylim=c(0, 80)) +
-  ggtitle("Different Attack Rates, with Low Movement (zoomed)")
+attack_move <- sim_system(T = 200, N_Ht = base_N_Ht, rho_Ht = base_rho_Ht, 
+                          r = base_r, epsilon = base_epsilon, phi_H = base_phi_H, 
+                          T_HH = c(0.25, 0.25), alpha_H = c(0.01, 0.01), 
+                          K_H = base_K_H, base_kappa_H)
+supp2b <- plot_sim(attack_move) +
+  coord_cartesian(ylim=c(0, 100)) +
+  annotate(geom = "label", x = 150, y=90, 
+           label = "Equal Stable Attack Rate\nEqual Movement", 
+           fontface = "italic", size = 3)
 
-#Different attack rates, no movement----
-attack_nomove <- sim_system(T = 750, N_Ht = c(100, 100), rho_Ht = c(10, 10), 
-                          r = 0.1, epsilon = c(0.1, 0.1), phi_H = c(0.95, 0.95), 
-                          T_HH = c(0, 0), alpha_H = c(0.03, 0.06), 
-                          K_H = c(200, 200), kappa_H = c(25, 25))
+#Combine
+row2 <- plot_grid(
+  supp2a + theme(legend.position = "none"), 
+  supp2b + 
+    theme(legend.position = "none") +
+    ylab(" "),
+  labels = "auto")
 
-plot_sim(attack_nomove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  ggtitle("Different Attack Rates, without Movement")
+supp2 <- plot_grid(
+  row2, leg,
+  ncol = 1, rel_heights = c(1, .1)
+)
 
-#Zoom
-plot_sim(attack_nomove) +
-  geom_hline(yintercept = 200, linetype = "dashed", color = "gray40", size = 1) +
-  annotate(geom = "text", x = 500, y = 190, label = expression(K[A] * "=" * K[B] * "=" * 200)) +
-  coord_cartesian(ylim=c(0, 80)) +
-  ggtitle("Different Attack Rates, without Movement (zoomed)")
+ggsave(plot = supp2, filename = "fig/supp2.tiff", width = 6.5, height = 3.5, units = "in",
+       dpi = 200, compression = "lzw")
